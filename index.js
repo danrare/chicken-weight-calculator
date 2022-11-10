3736b25e70285035b6eca15c131500925128203c
danrare <61440086+danrare@users.noreply.github.com>
2022-11-10 06:54:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function (){
+    let weights = []
+
+    let total = 0;
+
+    let count = 0;
+
+    document.getElementById("add-el").disabled = true;
+
+    document.getElementById("average-el").disabled = true;
+
+    if (document.getElementById("add-el").disabled === true){
+        document.getElementById("add-el").classList.add("disabled");
+
+        if(document.getElementById("average-el").disabled === true){
+            document.getElementById("average-el").classList.add("disabled")
+        }
+    }
+
+    document.getElementById("weight-el").onkeyup = ()=>{
+        document.getElementById("add-el").disabled = false;
+
+        document.getElementById("add-el").classList.remove("disabled");
+
+        document.getElementById("add-el").onclick = ()=>{
+            weights.push(Number(document.getElementById("weight-el").value));
+
+            let li = document.createElement("li");
+
+            li.innerHTML = document.getElementById("weight-el").value;
+
+            document.querySelector("ul").append(li);
+    
+            console.log(weights);
+    
+            document.getElementById("weight-el").value = '';
+    
+            document.getElementById("add-el").disabled = true;
+            
+            document.getElementById("add-el").classList.add("disabled");
+        }
+    }
+
+    document.getElementById("total-el").onclick = ()=>{
+        for (let i = 0; i<weights.length; i++){
+            total+=weights[i];
+        }
+        document.getElementById("tot").textContent = "Total weight is: " + total + " kg";
+
+        document.getElementById("average-el").disabled = false;
+
+        document.getElementById("average-el").classList.remove("disabled");
+
+    }
+
+
+    document.getElementById("average-el").onclick = ()=>{
+        let average = Math.round(total/weights.length);
+        document.getElementById("avg").textContent = "Average weight is: " + average + " kg";
+    }
+    
+})
