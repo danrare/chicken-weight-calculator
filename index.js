693699336660999b6867babc4557b14ee23af927
danrare <61440086+danrare@users.noreply.github.com>
2022-11-12 09:39:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,14 +8,24 @@ document.addEventListener("DOMContentLoaded", function (){
     document.getElementById("add-el").disabled = true;
 
     document.getElementById("average-el").disabled = true;
+    
     document.getElementById("total-el").disabled = true;
 
+    document.getElementById("remove-el").disabled = true;
 
     if (document.getElementById("add-el").disabled === true){
         document.getElementById("add-el").classList.add("disabled");
 
         if(document.getElementById("average-el").disabled === true){
             document.getElementById("average-el").classList.add("disabled")
+        }
+
+        if (document.getElementById("total-el").disabled === true){
+            document.getElementById("total-el").classList.add("disabled");
+        }
+
+        if (document.getElementById("remove-el").disabled === true){
+            document.getElementById("remove-el").classList.add("disabled");
         }
     }
 
@@ -33,24 +43,56 @@ document.addEventListener("DOMContentLoaded", function (){
 
             document.querySelector("ul").append(li);
     
+            console.log(weights);
     
             document.getElementById("weight-el").value = '';
     
             document.getElementById("add-el").disabled = true;
-            document.getElementById("total-el").disabled = false;
+
             document.getElementById("add-el").classList.add("disabled");
+
+            document.getElementById("remove-el").disabled = false;
+
+            document.getElementById("remove-el").classList.remove("disabled");
+
+            document.getElementById("total-el").disabled = false;
+
+            document.getElementById("total-el").classList.remove("disabled");
         }
     }
 
+    document.getElementById("remove-el").onclick = ()=>{
+        weights.pop();
+        document.querySelector("li").remove();
+
+        if(weights.length === 0){
+            document.getElementById("remove-el").disabled = true;
+        }
+        console.log(weights);
+    }
+
     document.getElementById("total-el").onclick = ()=>{
-        for (let i = 0; i<weights.length; i++){
+
+        if (weights.length !== 0){
+            for (let i = 0; i<weights.length; i++){
             total+=weights[i];
+
+            // total = weights[i];
         }
         document.getElementById("tot").textContent = "Total weight is: " + total + " g";
+
+        // if(weights === []){
+        //     alert("Error, can't take value of zero")
+        // }
 
         document.getElementById("average-el").disabled = false;
 
         document.getElementById("average-el").classList.remove("disabled");
+        }else {
+            alert("Error, can't take value of zero")
+        }
+        
+
 
     }
 
@@ -58,6 +100,10 @@ document.addEventListener("DOMContentLoaded", function (){
     document.getElementById("average-el").onclick = ()=>{
         let average = Math.round(total/weights.length);
         document.getElementById("avg").textContent = "Average weight is: " + average + " g";
+    }
+
+    document.getElementById("reset-el").onclick= function reset(){
+        window.location.reload();
     }
     
 })
